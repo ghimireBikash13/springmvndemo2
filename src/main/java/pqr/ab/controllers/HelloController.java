e7fcd207b82854bd36885f274a2375706958e49b
Bikash Ghimire <bikashghimire@Bikashs-MacBook-Pro.local>
2020-02-18 15:50:13 -0500

    1234-springmvndemo2

---
+++ src/main/java/pqr/ab/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package pqr.ab.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	
+	
+	@RequestMapping(value="hello", method = RequestMethod.GET)
+	public void showHelloPage() {
+		System.out.println("");
+		
+	}
+
+}
